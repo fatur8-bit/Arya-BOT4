4c1c9b243b29a72ff45485ef114a233bfaef0d99
fatur8-bit <78153603+fatur8-bit@users.noreply.github.com>
2021-03-05 19:12:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,19 +1,19 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Smartfren [088235435804]
-│ • Telkomsel [081357302007]
-│ • Gopay [081357302007]
+│ • Axis [0859140514162]
+│ • Dqna [0859140514162]
+│ • Gopay [0859140514162]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281357302007
+│ > Ingin donasi? Wa.me/62859140514162
 ╰────
 
 Ini *#caranya untuk Donasi*
 *Cara Donasi*:
 1.) Beli ke pulsa/ konter terdekat semisal Indomaret
 2.) Bilang ke konter terdekat..
-"Beli pulsa telkomsel"
-3.)Dan terus masukkan nomor kami 081357302007
+"Beli pulsa axis"
+3.)Dan terus masukkan nomor kami 0859140514162
 4.) Jika sudah kirim bukti ke sini.. Terimakasih
 *Kalau tidak juga gak papa*👍🏻
 `.trim()) // Tambah sendiri kalo mau
